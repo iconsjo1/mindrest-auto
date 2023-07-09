3f282112c315d9853fe6641b7db510c35389ecd8
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-09 11:20:06 +0300

    polishing code

--- src/Modules/Public/Report/index.js
+++ src/Modules/Public/Report/index.js
@@ -2,4 +2,4 @@ module.exports = (app, db) => {
  // const routes =  routes
  require('./daily-report')('/REST/report-daily')(app, db);
  require('./payment')('/REST/payments')(app, db);
- };
+};
--- src/Modules/Public/Report/payment/index.js
+++ src/Modules/Public/Report/payment/index.js
@@ -6,7 +6,7 @@ module.exports = route => (app, db) => {
   try {
    const { id, limit } = req.query;
 
-   const {rows} = isPositiveInteger(id)
+   const { rows } = isPositiveInteger(id)
     ? await db.query('SELECT * FROM public."V_Payments" WHERE 1=1 AND id=$1', [id])
     : await db.query('SELECT * FROM public."V_Payments" ' + getLimitClause(limit));
 
