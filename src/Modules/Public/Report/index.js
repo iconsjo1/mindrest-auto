b09ad9332a24707e0bc9372e2636216f0cc391e2
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-09 11:18:18 +0300

    add payment

--- src/Modules/Public/Report/index.js
+++ src/Modules/Public/Report/index.js
@@ -1,7 +1,5 @@
 module.exports = (app, db) => {
  // const routes =  routes
  require('./daily-report')('/REST/report-daily')(app, db);
- /* require('./Create')(route)(app, db);
- require('./Update')(route)(app, db);
- require('./Delete')(route)(app, db);*/
-};
+ require('./payment')('/REST/payments')(app, db);
+ };
---
+++ src/Modules/Public/Report/payment/index.js
@@ -0,0 +1,23 @@
+const { isPositiveInteger, getLimitClause } = require('../../../../Utils');
+
+module.exports = route => (app, db) => {
+ // Read Relationship[s]
+ app.get(route, async (req, res) => {
+  try {
+   const { id, limit } = req.query;
+
+   const {rows} = isPositiveInteger(id)
+    ? await db.query('SELECT * FROM public."V_Payments" WHERE 1=1 AND id=$1', [id])
+    : await db.query('SELECT * FROM public."V_Payments" ' + getLimitClause(limit));
+
+   res.json({
+    success: true,
+    no_of_records: rows.length,
+    msg: `Relationship${1 === rows.length ? '' : 's'} retrieved successfully.`,
+    data: rows,
+   });
+  } catch ({ message }) {
+   res.json({ success: false, message });
+  }
+ });
+};
