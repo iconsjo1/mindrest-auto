5b120b7f6206a87a3196c422d048a75ce7600a82
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2023-07-17 12:49:08 +0300

    session

--- src/Modules/Public/Session/Read/index.js
+++ src/Modules/Public/Session/Read/index.js
@@ -1,20 +1,20 @@
-const { isPositiveInteger, getLimitClause } = require('../../../../Utils');
-
 module.exports = route => (app, db) => {
  // Read Session[s]
  app.get(route, async (req, res) => {
   try {
+   const { db, isPositiveInteger, orderBy, getLimitClause } = res.locals.utils;
+
    const { id, limit } = req.query;
 
-   const Sessions = isPositiveInteger(id)
+   const { rows } = isPositiveInteger(id)
     ? await db.query('SELECT * FROM public."Sessions" WHERE 1=1 AND id=$1', [id])
-    : await db.query('SELECT * FROM public."Sessions" ' + getLimitClause(limit));
+    : await db.query(`SELECT * FROM public."Sessions" ${orderBy('id')} ${getLimitClause(limit)}`);
 
    res.json({
     success: true,
-    no_of_records: Sessions.rows.length,
-    msg: `Session${1 === Sessions.rows.length ? '' : 's'} retrieved successfully.`,
-    data: Sessions.rows.reverse(),
+    no_of_records: rows.length,
+    msg: `Session${1 === rows.length ? ' was' : 's were'} retrieved successfully.`,
+    data: rows,
    });
   } catch ({ message }) {
    res.json({ success: false, message });
--- src/Modules/Public/Session/Update/index.js
+++ src/Modules/Public/Session/Update/index.js
@@ -1,24 +1,23 @@
-const { isPositiveInteger } = require('../../../../Utils');
-
 module.exports = route => (app, db) => {
  // Update Session
  app.put(route, async (req, res) => {
   try {
+   const { db, isPositiveInteger } = res.locals.utils;
+
    const { id } = req.query;
    if (!isPositiveInteger(id))
     return res.status(404).json({ Success: false, msg: 'Session not found.' });
 
    const changed = [];
-
    let i = 1;
    for (let prop in req.body) changed.push(`${prop} = $${i++}`);
 
-   const modifiedSession = await db.query(
+   const { rows } = await db.query(
     `UPDATE public."Sessions" SET ${changed} WHERE 1=1 AND id=$${i} RETURNING *`,
     [...Object.values(req.body), id]
    );
 
-   res.json({ success: true, msg: 'Session updated successfully.', data: modifiedSession.rows });
+   res.json({ success: true, msg: 'Session updated successfully.', data: rows });
   } catch ({ message }) {
    res.json({ success: false, message });
   }
