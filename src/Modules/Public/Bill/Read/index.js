feb5e3424f8e2a048c1bf8723556f2539b21eb13
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-17 13:08:53 +0300

    Bill update @wasim

--- src/Modules/Public/Bill/Read/index.js
+++ src/Modules/Public/Bill/Read/index.js
@@ -1,9 +1,9 @@
-const { isPositiveInteger, getLimitClause, orderBy } = require('../../../../Utils');
-
 module.exports = route => (app, db) => {
  // Read Bill[s]
  app.get(route, async (req, res) => {
   try {
+
+    const { db,isPositiveInteger, getLimitClause, orderBy } =res.locals.utils;
    const { id, limit } = req.query;
 
    const { rows } = isPositiveInteger(id)
