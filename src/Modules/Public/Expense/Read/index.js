ccb9a4113fc8b9c506fee747da75d1959170e115
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2023-07-18 15:00:27 +0300

    Expense polished

--- src/Modules/Public/Expense/Read/index.js
+++ src/Modules/Public/Expense/Read/index.js
@@ -2,16 +2,17 @@ module.exports = route => (app, db) => {
  // Read Expense[s]
  app.get(route, async (req, res) => {
   try {
-   const { db, isPositiveInteger, getLimitClause, orderBy } = res.locals.utils;
+   const { db, isPositiveInteger, getLimitClause } = res.locals.utils;
    const { id, limit } = req.query;
 
    const { rows } = isPositiveInteger(id)
     ? await db.query('SELECT * FROM public."V_Expenses" WHERE 1=1 AND id=$1', [id])
-    : await db.query(`SELECT * FROM public."V_Expenses" ${orderBy('id')} ${getLimitClause(limit)}`);
+    : await db.query(`SELECT * FROM public."V_Expenses" ${getLimitClause(limit)}`);
 
    res.json({
     success: true,
-    msg: `Expense${1 === rows.length ? '' : 's'} retrieved successfully.`,
+    no_of_records: rows.length,
+    msg: `Expense${1 === rows.length ? ' was' : 's were'} retrieved successfully.`,
     data: rows,
    });
   } catch ({ message }) {
