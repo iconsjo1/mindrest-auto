732e42f7896b30170475338c3b6620290b837854
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-19 12:43:49 +0300

    app.use to  Holiday
     Lab_Test
     Lab
     Marital_Status
     Medicine
     Patient
     Personnel_Title
     Prescription

--- src/Modules/Public/Medicine/index.js
+++ src/Modules/Public/Medicine/index.js
@@ -1,6 +1,10 @@
 const routes = { medicine: '/REST/medicines', mtf: '/REST/medicine_to_foods' };
 module.exports = app => {
  const { medicine, mtf } = routes;
+ const logger = require('../../../Utils/Route_Logger');
+
+ app.use(medicine, logger);
+ app.get(mtf, logger);
  require('./Read')({ medicine, mtf })(app);
  require('./Create')(medicine)(app);
  require('./Update')(medicine)(app);
--- src/Modules/Public/Patient/Home/index.js
+++ src/Modules/Public/Patient/Home/index.js
@@ -1,4 +1,5 @@
 module.exports = route => app => {
+    app.use(route, require('../../../Utils/Route_Logger'));
  require('./Read')(route)(app);
  require('./Create')(route.patients)(app);
  require('./Update')(route.patients)(app);
