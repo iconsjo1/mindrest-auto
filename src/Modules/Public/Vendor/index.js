a7d7b038a169c2aab01357180810d39c55a2df68
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-19 12:09:29 +0300

    app.use to Screen &Screen_Group &Session &Vendor

--- src/Modules/Public/Vendor/index.js
+++ src/Modules/Public/Vendor/index.js
@@ -1,6 +1,8 @@
 const route = '/Rest/vendors';
 
 module.exports = (app, db) => {
+app.use(route, require('../../../Utils/Route_Logger'));
+
  require('./Read')(route)(app, db);
  require('./Create')(route)(app, db);
  require('./Update')(route)(app, db);
