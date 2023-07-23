b7eb8701cd80e6a2998b574b44e8aa4fb97ee5f1
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2023-07-23 11:36:39 +0300

    user post polished

--- src/Modules/User/Create/all.js
+++ src/Modules/User/Create/all.js
@@ -2,14 +2,19 @@ module.exports = route => app => {
  // Create User
  app.post(route, async (req, res) => {
   try {
+   const { db, isEString } = res.locals.utils;
+
    const { email } = req.body;
-   if (email) {
-    const user = await db.query(
-     'SELECT CONCAT_WS(\' \', user_first_name, user_middle_name, user_last_name) name, id FROM public."Users" WHERE 1=1 AND email = $1',
+   if (!isEString(email))
+    return res.status(400).json({ success: false, msg: 'email can only be a string or empty' });
+
+   if (0 < email.length) {
+    const { rows: users } = await db.query(
+     'SELECT id FROM public."Users" WHERE 1=1 AND email = $1',
      [email]
     );
 
-    if (0 === user.rows.length) {
+    if (0 === users.length) {
      // Email is unique do insert
 
      const fields = Object.keys(req.body);
@@ -18,40 +23,39 @@ module.exports = route => app => {
 
      for (let i = 0; i < values.length; enc_values.push(`$${++i}`));
 
-     const newUser = await db.query(
+     const { rows } = await db.query(
       `INSERT INTO public."Users"(${fields}) VALUES(${enc_values}) RETURNING *`,
       values
      );
 
-     return res.json({ success: true, msg: 'User created successfully.', data: newUser.rows });
+     return res.json({ success: true, msg: 'User was created successfully.', data: rows });
     }
     // Email is not unique and/or it may be used.
-    const { id: user_id, name: user_actual_name } = user.rows[0];
+    const { id: user_id } = users[0];
 
-    if (user_id) {
-     const patient = await db.query('SELECT id from public."Patients" WHERE 1=1 AND user_id = $1', [
-      user_id,
-     ]);
-     if (1 === patient.rows.length) {
-      return res.json({
-       success: false,
-       msg: `Duplicate email for patient (${user_actual_name}) with id (${patient.rows[0].id})`,
-      });
-     }
-     const doctor = await db.query('SELECT id from public."Doctors" WHERE 1=1 AND user_id = $1', [
-      user_id,
-     ]);
-     if (1 === doctor.rows.length) {
-      return res.json({
-       success: false,
-       msg: `Duplicate email for doctor (${user_actual_name}) with id (${doctor.rows[0].id})`,
-      });
-     }
-     return res.json({
-      success: false,
-      msg: `This user (${user_actual_name}) id=(${user_id}) email not unique or isn\'t assigned to a patient/doctor`,
-     });
-    }
+    const { rows: notuniqueuser } = await db.query(
+     `SELECT id,
+             'Paitient'
+            FROM public."Patients" 
+           WHERE user_id = $1
+        UNION
+        SELECT id,
+               'Doctor'
+              FROM public."Doctors"
+             WHERE user_id =$1 AND FALSE = is_therapist
+        UNION
+        SELECT id,
+               'Therapist' type
+              FROM public."Doctors" 
+             WHERE user_id =$1 AND TRUE = is_therapist`.replace(/\s+/g, ' '),
+     [user_id]
+    );
+    res.json({
+     success: false,
+     msg: `email is not unique and user_id={${user_id}} ${
+      0 < notuniqueuser.length ? `used as a ${notuniqueuser.type}` : ''
+     })`,
+    });
    }
   } catch ({ message }) {
    res.json({ success: false, message });
