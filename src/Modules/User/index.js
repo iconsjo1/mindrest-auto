5380a149e8149d942bf8cf1a94dfc90ae62d352a
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-07-30 15:05:49 +0300

    update user login

--- src/Modules/User/index.js
+++ src/Modules/User/index.js
@@ -1,14 +1,47 @@
-const routes = { all: '/REST/users', login: '/REST/user/login' };
+const auth = require('../../Utils/Auth');
+
+const routes = {
+ all: '/REST/users',
+ login: '/REST/user/login',
+ logout: '/REST/user-logout',
+};
 
 module.exports = app => {
- const { all, login } = routes;
- const logger = require('../../Utils/Route_Logger');
+ const { all, login, logout } = routes;
 
- app.use(all, logger);
- app.post(login, logger);
+ app.post(all, auth);
+ app.delete(logout, auth);
+ app.delete(all, auth);
+ app.get(all, auth);
+ app.put(all, auth);
+
+ app.use(
+  [all, login, logout],
+  [
+   require('../../Utils/Route_Logger'),
+   (_, res, next) => {
+    res.locals.user_columns = [
+     'id',
+     'title_id',
+     'user_first_name',
+     'user_middle_name',
+     'user_last_name',
+     'dob',
+     'email',
+     'user_contact_number',
+     'country_id',
+     'avatar_id',
+     'role_id',
+     'password',
+     'jwt_token',
+    ];
+    next();
+   },
+  ]
+ );
 
  require('./Read')(all)(app);
  require('./Create')({ all, login })(app);
  require('./Update')(all)(app);
- require('./Delete')(all)(app);
+ require('./Delete')({ all, logout })(app);
 };
