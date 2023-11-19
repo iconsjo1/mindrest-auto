026948fef7cad46a57a8808ef8bc17c55b0ac987
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-11-19 12:59:56 +0200

    remove Service_Provider

--- src/Modules/Service/Service_Provider/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = route => app => {
- require('./Read')(route)(app);
- require('./Create')(route)(app);
- require('./Update')(route)(app);
- require('./Delete')(route)(app);
-};
--- src/Modules/Service/index.js
+++ src/Modules/Service/index.js
@@ -1,15 +1,14 @@
 const routes = {
  services: '/REST/services',
  servicec: '/REST/service_categories',
- servicep: '/REST/service_providers',
 };
 
 module.exports = app => {
- const { services, servicec, servicep } = routes;
+ const { services, servicec } = routes;
 
- app.use([services, servicec, servicep], require('../../Utils/Route_Logger'));
+ app.use([services, servicec], require('../../Utils/Route_Logger'));
 
  require('./Home')(services)(app);
  require('./Service_Category')(servicec)(app);
- require('./Service_Provider')(servicep)(app);
+ //require('./Service_Provider')(servicep)(app);
 };
