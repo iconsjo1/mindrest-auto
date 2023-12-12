163e7a98e144e7fd73e947d45122e1366ebd299c
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2023-12-12 13:21:50 +0200

    Add view to Bill

--- src/Modules/Bill/Read/index.js
+++ src/Modules/Bill/Read/index.js
@@ -2,12 +2,12 @@ module.exports = route => app => {
  // Read Bill[s]
  app.get(route, async (req, res) => {
   try {
-   const { db, isPositiveInteger, getLimitClause, orderBy } = res.locals.utils;
+   const { db, isPositiveInteger, getLimitClause } = res.locals.utils;
    const { id, limit } = req.query;
 
    const { rows } = isPositiveInteger(id)
-    ? await db.query('SELECT * FROM public."Bills" WHERE 1=1 AND id=$1', [id])
-    : await db.query(`SELECT * FROM public."Bills" ${orderBy('id')} ${getLimitClause(limit)}`);
+    ? await db.query('SELECT * FROM public."V_Bills" WHERE 1=1 AND id=$1', [id])
+    : await db.query(`SELECT * FROM public."V_Bills" ${getLimitClause(limit)}`);
 
    res.json({
     success: true,
