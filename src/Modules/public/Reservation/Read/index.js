7fdcf192ec81ba23f6cbf26628e24c758ddce45b
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2024-03-11 14:28:36 +0300

    reservation now restricts doctor

--- src/Modules/public/Reservation/Read/index.js
+++ src/Modules/public/Reservation/Read/index.js
@@ -2,12 +2,29 @@ module.exports = route => app => {
  // Read Reservation[s]
  app.get(route, async (req, res) => {
   try {
-   const { db, isPositiveInteger, getLimitClause } = res.locals.utils;
-   const { id, limit } = req.query;
+   const { role_id, doctor_id: DBdoctor_id } = res.locals;
+   const { db, isPositiveInteger, orderBy, getLimitClause, ROLES } = res.locals.utils;
 
-   const { rows } = isPositiveInteger(id)
-    ? await db.query('SELECT * FROM public."V_Reservations" WHERE 1=1 AND id=$1', [id])
-    : await db.query(`SELECT * FROM public."V_Reservations" ${getLimitClause(limit)}`);
+   const { id, patient_id, doctor_id, limit } = req.query;
+   const filters = { id, doctor_id, patient_id };
+
+   if (ROLES.DOCTOR === role_id) filters['doctor_id'] = DBdoctor_id;
+
+   const conditionSet = ['1=1'];
+   const valueSet = [];
+
+   for (let i = 0, sanitizingIndex = 0, keys = Object.keys(filters); i < keys.length; i++) {
+    const k = keys[i];
+    const v = filters[k];
+    if (isPositiveInteger(v)) {
+     conditionSet.push(`${k} = $${++sanitizingIndex}`);
+     valueSet.push(v);
+    }
+   }
+   const { rows } = await db.query(
+    `SELECT * FROM public."V_Reservations" WHERE ${conditionSet.join(' AND ')} ${orderBy('id')} ${getLimitClause(limit)}`,
+    valueSet
+   );
 
    res.json({
     success: true,
