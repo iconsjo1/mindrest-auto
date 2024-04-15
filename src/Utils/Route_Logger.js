ae764e82a6b3e6c219844f2fec2dcea90d1760fa
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2024-04-15 12:06:45 +0300

    Update Route_Logger.js
    Route logger wont insert new routes

--- src/Utils/Route_Logger.js
+++ src/Utils/Route_Logger.js
@@ -1,6 +1,6 @@
 const pool = require('../pool');
 
-module.exports = (_, res, next) => {
+module.exports = async (_, res, next) => {
  const {
   req: {
    _parsedUrl: { pathname },
@@ -8,22 +8,13 @@ module.exports = (_, res, next) => {
   },
  } = res;
 
- pool
-  .query({
-   name: 'route_select',
-   text: 'SELECT 1 FROM public."Rest_Routes" WHERE method = $1 AND route = $2',
-   values: [method, pathname],
-   rowMode: 'array',
-  })
-  .then(({ rows }) => {
-   if (0 === rows.length) {
-    pool
-     .query({
-      name: 'route_insert',
-      text: 'INSERT INTO public."Rest_Routes"(method, route) SELECT $1, $2',
-      values: [method, pathname],
-     })
-     .then(_ => next());
-   } else next();
-  });
+ const { rows } = await pool.query({
+  name: 'route_select',
+  text: 'SELECT 1 FROM public."Rest_Routes" WHERE method = $1 AND route = $2',
+  values: [method, pathname],
+  rowMode: 'array',
+ });
+
+ if (0 === rows.length) res.json({ success: false, message: 'Resourse was not found.' });
+ else next();
 };
