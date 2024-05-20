f7d85ccca3f243364d699ed365ce19adfd0209f1
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2024-05-20 15:30:35 +0300

    catching promise

--- src/Utils/Route_Logger.js
+++ src/Utils/Route_Logger.js
@@ -8,12 +8,16 @@ module.exports = async (_, res, next) => {
   },
  } = res;
 
- const { rows } = await pool.query({
-  name: 'route_select',
-  text: 'SELECT 1 FROM public."Rest_Routes" WHERE method = $1 AND route = $2',
-  values: [method, pathname],
-  rowMode: 'array',
- });
+ const { rows } = await pool
+  .query({
+   name: 'route_select',
+   text: 'SELECT 1 FROM public."Rest_Routes" WHERE method = $1 AND route = $2',
+   values: [method, pathname],
+   rowMode: 'array',
+  })
+  .catch(({ message }) => {
+   throw Error(message);
+  });
 
  if (0 === rows.length) res.json({ success: false, message: 'Resourse was not found.' });
  else next();
