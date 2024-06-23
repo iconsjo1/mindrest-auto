643e691a039f77238dc40e57cf268d03cf350e2a
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-06-23 15:03:09 +0300

    Update ObjectArrayCheck.js

--- src/Utils/ObjectArrayCheck.js
+++ src/Utils/ObjectArrayCheck.js
@@ -1,6 +1,8 @@
 module.exports = (arr, everycb) => mayEmpty => {
- if ('function' !== typeof everycb) return false;
- if (true !== mayEmpty || null == arr || 0 === arr?.length) return true;
-
- return Array.isArray(arr) && 0 < arr.length && arr.every(everycb);
-};
+    if ('function' !== typeof everycb) return false;
+   
+    if (true === mayEmpty && (null == arr || 0 === arr?.length)) return true;
+   
+    return Array.isArray(arr) && 0 < arr.length && arr.every(everycb);
+   };
+   
