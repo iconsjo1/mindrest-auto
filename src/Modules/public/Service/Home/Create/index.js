6e8270d26d703be8bc9046d29ddbd96c0856a116
icon-ramico <97400721+icon-ramico@users.noreply.github.com>
2024-09-22 13:37:00 +0300

    feat: connecting service to erp

--- src/Modules/public/Service/Home/Create/index.js
+++ src/Modules/public/Service/Home/Create/index.js
@@ -2,19 +2,28 @@ module.exports = route => app => {
  // Create Service
  app.post(route, async (req, res) => {
   try {
-   const { db } = res.locals.utils;
+   const {
+    db,
+    env: {
+     ERP: { SERVICE },
+    },
+   } = res.locals.utils;
 
    const fields = Object.keys(req.body);
    const values = Object.values(req.body);
-   const enc_values = [];
+   const enc_values = values.map((_, i) => `$${++i}`);
 
-   for (let i = 0; i < values.length; enc_values.push(`$${++i}`));
+   const dispDate = await db
+    .query(`INSERT INTO public."Services"(${fields}) VALUES(${enc_values}) RETURNING *`, values)
+    .then(async ({ rows }) => {
+     if (0 === rows.length) throw Error('Cannot insert service.');
+     return {
+      service: rows[0],
+      service_ref: await SERVICE.create(rows[0].service_ref),
+     };
+    });
 
-   const { rows } = await db.query(
-    `INSERT INTO public."Services"(${fields}) VALUES(${enc_values}) RETURNING *`,
-    values
-   );
-   res.json({ success: true, msg: 'Service was created successfully.', data: rows });
+   res.json({ success: true, msg: 'Service was created successfully.', data: dispDate });
   } catch ({ message }) {
    res.json({ success: false, message });
   }
