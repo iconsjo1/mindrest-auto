6b928921f26e469f1d485717f5fb53e4c0352685
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-01 14:09:15 +0300

    Update sell_invoice.js

--- src/Modules/ERP/report/sell_invoice.js
+++ src/Modules/ERP/report/sell_invoice.js
@@ -15,21 +15,21 @@ module.exports = route => app => {
    if (!isPositiveInteger(patient_id)) throw Error('patient_id should be positive integer.');
 
    const { rows: bills } = await db.query(
-    `SELECT  ROW_NUMBER() OVER(ORDER BY bill_id) AS ser,invoice_ref  ,bill_id,appointment_id,appointment_date,service_name,patient_name FROM public."V_Doctor_Patients" a
+    `SELECT ROW_NUMBER() OVER(ORDER BY bill_id) AS ser,invoice_ref  ,bill_id,appointment_id,appointment_date,service_name,patient_name FROM public."V_Doctor_Patients" a
     JOIN public."Bills" b ON b.id =a.bill_id 
         WHERE patient_id =$1 `,
     [patient_id]
    );
 
    const invoices = await Promise.all(bills.map(b => INVOICE.read(b.invoice_ref))).then(rows =>
-    rows.map(item => item.data)
+    rows.map(item => item.invoice_data)
    );
 
    const merged = bills
     .filter(b => invoices.some(inv => inv.name == b.invoice_ref))
     .map(b => {
-     const itemY = invoices.find(item => item.name == b.invoice_ref);
-     return { ...b, posting_date: itemY.posting_date, total: itemY.total };
+     const inv = invoices.find(item => item.name == b.invoice_ref);
+     return { ...b, posting_date: inv.posting_date, total: inv.total };
     });
 
    res.json({
