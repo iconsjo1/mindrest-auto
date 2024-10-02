9388a2def57c19d8c47fab46b7d84631b5bb1586
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-02 13:56:51 +0300

    Add filter by role_id in route user get

--- src/Modules/public/User/Common/Read/index.js
+++ src/Modules/public/User/Common/Read/index.js
@@ -4,11 +4,15 @@ module.exports = route => app => {
   try {
    const { db, isPositiveInteger, orderBy, getLimitClause } = res.locals.utils;
 
-   const { id, limit } = req.query;
+   const { id, role_id, limit } = req.query;
 
    const { rows } = isPositiveInteger(id)
     ? await db.query('SELECT * FROM public."Users" WHERE 1=1 AND id=$1 AND is_deleted =false', [id])
-    : await db.query(`SELECT * FROM public."Users" WHERE is_deleted =false ${orderBy('id')} ${getLimitClause(limit)}`);
+    : isPositiveInteger(role_id)
+      ? await db.query(` SELECT * FROM public."Users" WHERE 1=1 AND role_id=$1 AND is_deleted =false`, [role_id])
+      : await db.query(
+         `SELECT * FROM public."Users" WHERE is_deleted =false ${orderBy('id')} ${getLimitClause(limit)}`
+        );
 
    res.json({
     success: true,
