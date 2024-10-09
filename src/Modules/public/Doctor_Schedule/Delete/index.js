e1dd26cc297cd311c3ef1916f08fd2bf42cbc0bc
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-09 14:07:10 +0300

    update Doctor_Schedule delete by doctor_id, week_day_id

--- src/Modules/public/Doctor_Schedule/Delete/index.js
+++ src/Modules/public/Doctor_Schedule/Delete/index.js
@@ -5,21 +5,23 @@ module.exports = route => app => {
    const {
     locals: {
      utils: { db, isPositiveInteger, ROLES },
-     role_id,
-     doctor_id,
+     //  role_id,
+     //  doctor_id,
     },
    } = res;
-   const clause = ROLES.DOCTOR === role_id ? 'doctor_id= ' + doctor_id : '1=1';
+   //    const clause = ROLES.DOCTOR === role_id ? 'doctor_id= ' + doctor_id : '1=1';
 
-   const { id } = req.query;
-   if (!isPositiveInteger(id)) return res.status(404).json({ success: false, msg: 'Doctor schedule was not found.' });
+   const { doctor_id, week_day_id } = req.query;
+   if (!isPositiveInteger(doctor_id))
+    return res.status(404).json({ success: false, msg: 'Doctor schedule was not found.' });
+   if (!isPositiveInteger(week_day_id)) return res.status(404).json({ success: false, msg: 'Week day was not found.' });
 
    const { rows } = await db.query(
-    `DELETE FROM public."Doctor_Schedules" WHERE 1=1 AND ${clause} AND id = $1 RETURNING *`,
-    [id]
+    `DELETE FROM public."Doctor_Schedules" WHERE 1=1 AND doctor_id=$1 AND week_day_id=$2 RETURNING *`,
+    [doctor_id, week_day_id]
    );
 
-   res.json({ Success: true, msg: 'Doctor schedule was deleted successfully.', data: rows });
+   res.json({ success: true, msg: 'Doctor schedule was deleted successfully.', data: rows });
   } catch ({ message }) {
    res.json({ success: false, message });
   }
