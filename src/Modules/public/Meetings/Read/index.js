ffa054d2cc72427b648e6b0b9d501e46984fbf57
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-09 15:24:10 +0300

    Update index.js

--- src/Modules/public/Meetings/Read/index.js
+++ src/Modules/public/Meetings/Read/index.js
@@ -18,7 +18,7 @@ module.exports = route => app => {
     values
    );
    const dispaly = [];
-   if (ROLES.DOCTOR === role_id) {
+   if (ROLES.DOCTOR === role_id || ROLES.THERAPIST === role_id) {
     dispaly.push(
      rows.map(item => {
       return {
