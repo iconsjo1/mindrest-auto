afb307b8f8cae177e81f5a70a95e4296adb273b8
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-09 12:39:52 +0300

    make the update of Doctor_Schedule handle more than one object

--- src/Modules/public/Doctor_Schedule/Update/index.js
+++ src/Modules/public/Doctor_Schedule/Update/index.js
@@ -1,41 +1,66 @@
 module.exports = route => app => {
  // Update Doctor Schedule
  app.put(route, async (req, res) => {
+  let client = null;
+  let begun = false;
   try {
    const {
     locals: {
-     utils: { db, isPositiveInteger, ROLES },
-     role_id,
-     doctor_id,
-     therapist_id,
+     utils: { db, isPositiveInteger, ROLES, SQLfeatures },
+     //  role_id,
+     //  doctor_id: rdoctor_id,
+     //  therapist_id,
     },
    } = res;
 
-   const clause = (r => {
-    switch (r) {
-     case ROLES.DOCTOR:
-      return 'doctor_id= ' + doctor_id;
-     case ROLES.THERAPIST:
-      return 'doctor_id= ' + therapist_id;
-     default:
-      return '1=1';
-    }
-   })(role_id);
+   //  const clause = (r => {
+   //   switch (r) {
+   //    case ROLES.DOCTOR:
+   //     return 'doctor_id= ' + rdoctor_id;
+   //    case ROLES.THERAPIST:
+   //     return 'doctor_id= ' + therapist_id;
+   //    default:
+   //     return '1=1';
+   //   }
+   //  })(role_id);
 
-   const { id } = req.query;
-   if (!isPositiveInteger(id)) return res.status(404).json({ Success: false, msg: 'Doctor schedule was not found.' });
+   const { id: doctor_id } = req.query;
+   const { week_day_id, time_table } = req.body;
 
-   let i = 1;
-   const changed = [];
-   for (let prop in req.body) changed.push(`${prop} = $${i++}`);
+   if (!isPositiveInteger(doctor_id))
+    return res.status(404).json({ success: false, msg: 'Doctor schedule was not found.' });
+   if (!isPositiveInteger(week_day_id)) return res.status(404).json({ success: false, msg: 'Week day was not found.' });
 
-   const { rows } = await db.query(
-    `UPDATE public."Doctor_Schedules" SET ${changed} WHERE 1=1 AND ${clause} AND id=$${i} RETURNING *`,
-    [...Object.values(req.body), id]
-   );
-   res.json({ success: true, msg: 'Doctor schedule was updated successfully.', data: rows });
+   const { fields, rows, values } = SQLfeatures.bulkInsert(time_table, { doctor_id, week_day_id });
+
+   client = await db.connect();
+   await client.query('BEGIN').then(() => (begun = true));
+
+   await client.query(`DELETE FROM public."Doctor_Schedules" WHERE doctor_id =$1 AND week_day_id=$2 `, [
+    doctor_id,
+    week_day_id,
+   ]);
+
+   const data = await client
+    .query(`INSERT INTO public."Doctor_Schedules"(${fields}) VALUES${rows} RETURNING *`, values)
+    .then(({ rows }) => rows);
+
+   await client.query('COMMIT').then(() => (begun = false));
+
+   res.json({ success: true, msg: 'Doctor schedule was updated successfully.', data });
   } catch ({ message }) {
    res.json({ success: false, message });
+  } finally {
+   if (null != client) {
+    if (true === begun) {
+     try {
+      await client.query('ROLLBACK');
+     } catch ({ message: rmessage }) {
+      throw Error(rmessage);
+     }
+    }
+    client.release();
+   }
   }
  });
 };
