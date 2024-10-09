c83f234c15fe5551c975e12b2be9ec4c3ffdcbe6
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-10-09 15:18:44 +0300

    Update index.js

--- src/Modules/public/Meetings/Read/index.js
+++ src/Modules/public/Meetings/Read/index.js
@@ -6,10 +6,10 @@ module.exports = route => app => {
 
    const { db, getLimitClause, SQLfeatures, ROLES } = res.locals.utils;
 
-//    const { role_id, user_id } = res.locals;
+   const { role_id, user_id } = res.locals;
 
-//    if (ROLES.DOCTOR === role_id) ids.user_id = user_id;
-//    if (ROLES.THERAPIST === role_id) ids.user_id = user_id;
+   //    if (ROLES.DOCTOR === role_id) ids.user_id = user_id;
+   //    if (ROLES.THERAPIST === role_id) ids.user_id = user_id;
 
    const { filters, values } = SQLfeatures.IDFilters(ids);
    console.log(`SELECT * FROM public."V_Meetings" WHERE ${filters} ${getLimitClause(limit)}`);
@@ -17,12 +17,24 @@ module.exports = route => app => {
     `SELECT * FROM public."V_Meetings" WHERE ${filters} ${getLimitClause(limit)}`,
     values
    );
-
+   const dispaly = [];
+   if (ROLES.DOCTOR === role_id) {
+    dispaly.push(
+     rows.map(item => {
+      return {
+       ...item,
+       show: item.user_id == user_id ? true : false,
+      };
+     })
+    );
+   } else {
+    dispaly = [...rows];
+   }
    res.json({
     success: true,
-    no_of_records: rows.length,
-    msg: `Meeting${1 === rows.length ? ' was' : 's were'} retrieved successfully.`,
-    data: rows,
+    no_of_records: dispaly.length,
+    msg: `Meeting${1 === dispaly.length ? ' was' : 's were'} retrieved successfully.`,
+    data: dispaly,
    });
   } catch ({ message }) {
    res.json({ success: false, message });
